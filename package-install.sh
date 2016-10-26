6158824bc062b8d6d98814170157a3943e25f459
Admin <james.s.nduka@synwin.co.uk>
2016-10-26 11:46:57 +0100

    Add net-tools sudo instgall to install/provision .sh script

--- package-install.sh
+++ package-install.sh
@@ -7,3 +7,4 @@ sudo yum install wget -y
 wget https://packages.chef.io/stable/el/7/chef-12.15.19-1.el7.x86_64.rpm -O /tmp/chef-12.15.19-1.el7.x86_64.rpm
 sudo rpm -ivh /tmp/chef-12.15.19-1.el7.x86_64.rpm
 sudo rm -rf /tmp/chef-12.15.19-1.el7.x86_64.rpm /tmp/go1.7.1.linux-amd64.tar.gz
+sudo yum install net-tools -y
